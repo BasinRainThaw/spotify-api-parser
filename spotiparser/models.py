2c24f6028f6a26ee901d12d84ed058e483ada159
BasinRainThaw <306958132+BasinRainThaw@users.noreply.github.com>
2026-05-19 22:24:29 +0300

    initial models setup (cont.)

--- spotiparser/models.py
+++ spotiparser/models.py
@@ -6,3 +6,11 @@ class Artist(BaseModel):
     name: str
     url: Optional[HttpUrl] = None
 
+class Track(BaseModel):
+    id: str
+    name: str
+    artists: List[Artist]
+    duration_ms: int
+    play_count: Optional[int] = 0
+    is_explicit: bool = False
+    album_name: Optional[str] = None
