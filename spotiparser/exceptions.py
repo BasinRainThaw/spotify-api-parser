260aefd724a52920241f9fdc4922cdd38e877288
BasinRainThaw <306958132+BasinRainThaw@users.noreply.github.com>
2026-06-02 01:43:34 +0300

    add base exceptions (cont.)

--- spotiparser/exceptions.py
+++ spotiparser/exceptions.py
@@ -2,3 +2,8 @@ class SpotiparserError(Exception):
     """Base exception for everything here"""
     pass
 
+class RateLimitError(SpotiparserError):
+    def __init__(self, message="Spotify is blocking us (429)", retry_after=None):
+        self.retry_after = retry_after
+        super().__init__(message)
+
