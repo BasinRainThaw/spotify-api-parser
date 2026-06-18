4d412cced69b938d322d52beffef306f929a47cf
BasinRainThaw <306958132+BasinRainThaw@users.noreply.github.com>
2026-06-19 00:58:46 +0300

    wip: regex hell for data extraction (cont.)

--- spotiparser/parser.py
+++ spotiparser/parser.py
@@ -10,3 +10,10 @@ def extract_json_blob(html: str):
         raise ScraperError("Could not find initial-state script tag")
     return json.loads(match.group(1))
 
+def parse_track(data: dict) -> Track:
+    return Track(
+        id=data.get('id'),
+        name=data.get('name'),
+        artists=[Artist(id=a['id'], name=a['name']) for a in data.get('artists', [])],
+        duration_ms=data.get('duration_ms', 0)
+    )
