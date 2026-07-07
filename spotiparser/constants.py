ec4724545fd5baaef0899b11dbb561fd34a626cc
BasinRainThaw <306958132+BasinRainThaw@users.noreply.github.com>
2026-07-08 00:00:53 +0300

    add more specific regex for track IDs

--- spotiparser/constants.py
+++ spotiparser/constants.py
@@ -7,3 +7,9 @@ DEFAULT_HEADERS = {
 
 # we need this to find the hydrated state in the html source
 STATE_RE = r'<script id="initial-state" type="application/json">(.*?)</script>'
+
+TRACK_ID_RE = r"spotify:track:([a-zA-Z0-9]{22})"
+PLAYLIST_ID_RE = r"spotify:playlist:([a-zA-Z0-9]{22})"
+
+# sometimes spotify uses a different script tag id depending on the region/rollout
+# TODO: check if 'session-state' is ever used instead of 'initial-state'
